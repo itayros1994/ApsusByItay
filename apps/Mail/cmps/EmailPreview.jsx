e6012412efc9b72c78296bd3822d19eabcf16d73
itay rosental <briskeritay3@gmail.com>
2021-05-07 18:08:45 +0300

    asdasd

--- apps/Mail/cmps/EmailPreview.jsx
+++ apps/Mail/cmps/EmailPreview.jsx
@@ -18,7 +18,7 @@ export function EmailPreview({ email, onDeleteMail }) {
         <div className="email-preview-container">
             <Link onClick={onEmailClicked} to={`/mail/${email.id}`}>
                 <div className={email.isRead ? "email-content" : "email-content email-read-bgc"} >
-                    <div className="email-send-by">From : {email.sendBy}</div>
+                    <div className="email-send-by"><span className="from">From : </span>{email.sendBy}</div>
                     <div className="email-subject"><span className="email-subject-prev">{email.subject}</span> : <span className="email-body-prev">{email.body.substring(0, 10)}</span></div>
                     <div className="email-sentAt">{email.sentAt}</div>
                     {/* <Link onClick={onEmailClicked} to={`/mail/${email.id}`}>Read Email</Link> */}
